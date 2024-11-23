bb95a0d641129caeaec83819c56ab91b74c9535c
Fatih S. Kalemci <fatihskalemci@gmail.com>
2024-11-23 23:49:51 +0300

    Add Division method

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+
+
+int Division(int dividend, int divisor)
+{
+    if (dividend > 100 | dividend <= 0)
+        throw new ArgumentOutOfRangeException("Dividend must be between 0 and 100");
+    
+    if (dividend % divisor != 0)
+        throw new Exception("Quotient is not an integer");
+
+    int result = dividend / divisor;
+    return result;
+}
